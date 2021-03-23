77d29833a942a00f82b823dce012c82b6bcb3e5b
Jarick <jarick@live.ca>
2021-03-22 21:54:43 -0400

    Finished

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-# docx_to_pdf
+## Description
+A feature in Python that reads a form as the word document and generates the form in PDF format.  This feature will be able to insert field value into the PDF.  This feature will also be able to digitally sign the PDF. 
+## Requirements
+Python version: 3.7.10.
+
+**Note**:
+[PDFNetPython3](https://www.pdftron.com/blog/python/python3/) only supports Python 3.5 - 3.8 on Windows (x64, x86), Linux (x64, x86), and macOS.  
+Use [Conda](https://docs.conda.io/projects/conda/en/latest/user-guide/tasks/manage-environments.html#removing-an-environment) to create a virtual environment and install the required python version.
+
+## Packages installed 
+
+```bash
+certifi==2020.12.5
+lxml==4.6.3
+PDFNetPython3==8.1.0
+Pillow==8.1.2
+python-docx==0.8.10
+reportlab==3.5.66
+wincertstore==0.2
+
+```
+
+## Installation
+
+Use the package manager [pip](https://pip.pypa.io/en/stable/) to install required libraries.
+
+```bash
+pip install -r requirements.txt
+```
+## Test
+**test1.docx** and **test2.docx** should be created
+```bash
+python docx_to_pdf.py
+```
--- docx_to_pdf.py
+++ docx_to_pdf.py
@@ -1,11 +1,12 @@
 import sys
-import docx
+from PDFNetPython3 import *
 from reportlab.pdfgen import canvas
 from reportlab.pdfbase import pdfform
 from reportlab.lib.colors import white, black
+import docx
 
 
-class docx_to_pdf:
+class Docx:
     def __init__(self, filename):
         self.filename = filename
 
@@ -13,7 +14,7 @@ class docx_to_pdf:
         try:
             doc = docx.Document(self.filename)
 
-        except docx.opc.exceptions.PackageNotFoundError:
+        except:
             print("Could not open/read file:{}".format(self.filename))
             sys.exit()
         return doc.paragraphs
@@ -25,21 +26,23 @@ def create_pdf(fname):
     return c
 
 
-target = docx_to_pdf("test.docx").read()
-
-def add_form(input, na):
-    c = create_pdf("test_output.pdf")
+def convert_to_pdf(paragraphs, input_list, output_file):
+    
+    c = create_pdf(output_file)
     form = c.acroForm
+
+    # Initiate x, y coordinates
     start_x = 20
     start_y = 650
 
     label_lst = []
     label = ""
 
+    # 
     form_length = 0
 
-    index_value = 0
-    for paragraph in input:
+    input_index = 0
+    for paragraph in paragraphs:
         content = paragraph.text
         if len(content) > 0:
             for i in range(len(content)):
@@ -50,28 +53,33 @@ def add_form(input, na):
                     if i+1 < len(content):
                         # draw form and reset form_length
                         if content[i+1] == " ":
-                            form.textfield(value=str(na[index_value]), name=label_lst[-1], tooltip=label_lst[-1],
+                            try:
+                                form.textfield(value=str(input_list[input_index]), name=label_lst[-1], tooltip=label_lst[-1],
+                                               x=start_x, y=start_y, borderStyle='inset',
+                                               borderColor=black, fillColor=white,
+                                               width=form_length*12, height=17,
+                                               textColor=black, forceBorder=True)
+                                start_x += form_length*12+10
+                                # label_lst.append("blank:{}".format(form_length))
+                                form_length = 0
+                                input_index += 1
+                                # print(label_lst[-1])
+                            except:
+                                print("Something goes wrong with input ")
+                    # Reach the end of paragraph, create input field
+                    else:
+                        try:
+                            form.textfield(value=str(input_list[input_index]), name=label_lst[-1], tooltip=label_lst[-1],
                                            x=start_x, y=start_y, borderStyle='inset',
                                            borderColor=black, fillColor=white,
                                            width=form_length*12, height=17,
                                            textColor=black, forceBorder=True)
+                            # update next x-pos
+                            input_index += 1
                             start_x += form_length*12+10
-                            # label_lst.append("blank:{}".format(form_length))
-                            form_length = 0
-                            index_value += 1
-                            print(label_lst[-1])
-                    # Reach the end of paragraph, create input field
-                    else:
-                        form.textfield(value=str(na[index_value]), name=label_lst[-1], tooltip=label_lst[-1],
-                                       x=start_x, y=start_y, borderStyle='inset',
-                                       borderColor=black, fillColor=white,
-                                       width=form_length*12, height=17,
-                                       textColor=black, forceBorder=True)
-                        # update next x-pos
-                        index_value += 1
-                        start_x += form_length*12+10
-                        print(label_lst[-1])
-
+                            # print(label_lst[-1])
+                        except:
+                            print("Something goes wrong with input ")
                 # skip if it is a space
                 elif content[i] == " ":
                     continue
@@ -81,18 +89,51 @@ def add_form(input, na):
                     if i+1 < len(content):
                         if content[i+1] == " " or content[i+1] == "_":
                             c.drawString(start_x, start_y, label)
-                            start_x += len(label)*12
+                            start_x += len(label)*8
                             label_lst.append(label)
                             label = ""
             # reset start_x
             start_x = 20
         else:
             label_lst.append("  ")
-        # start new ling
+        # start a new line
         start_y -= 30
+    c.drawString(start_x,start_y,"Signature: ")    
     c.save()
+    #start a new line for signature field
+    start_y -= 20
+    add_signature_field(output_file,start_x,start_y)
 
+def add_signature_field(outpath,x,y):
+    doc = PDFDoc(outpath)
+    page1 = doc.GetPage(1)
+    # Create a new signature form field in the PDFDoc. The name argument is optional;
+    # leaving it empty causes it to be auto-generated. However, you may need the name for later.
+    # Acrobat doesn't show digsigfield in side panel if it's without a widget. Using a
+    # Rect with 0 width and 0 height, or setting the NoPrint/Invisible flags makes it invisible.
+    certification_sig_field = doc.CreateDigitalSignatureField("Signiture")
+    widgetAnnot = SignatureWidget.Create(
+        doc, Rect(x, y, x+150, y-50), certification_sig_field)
+    page1.AnnotPushBack(widgetAnnot)
 
-add_form(target, ["join", 32,"out"])
-# doc = docx.Document("test.docx")
-# print(type(doc.paragraphs[0].text))
+    # (OPTIONAL) Add an appearance to the signature field.
+    # img = Image.Create(doc.GetSDFDoc(), appearance_image_path)
+    # widgetAnnot.CreateSignatureAppearance(img)
+
+    # Prepare the document locking permission level to be applied upon document certification.
+    certification_sig_field.SetDocumentPermissions(
+        DigitalSignatureField.e_annotating_formfilling_signing_allowed)
+
+    # Save the PDFDoc. Once the method below is called, PDFNet will also sign the document using the information provided.
+    doc.Save(outpath, 0)
+
+if __name__ == "__main__":
+    # test1 single line form
+    #input_list [name, age]
+    target1 = Docx("test1.docx").read()
+    convert_to_pdf(target1, ["John", 30], "output1.pdf")
+
+    # test2
+    #input_list [name, age, phone, school]
+    target2 = Docx("test2.docx").read()
+    convert_to_pdf(target2, ["John", 30, 123123, "blabla"], "output2.pdf")
